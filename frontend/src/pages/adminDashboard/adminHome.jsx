842fbd65b05aac42b2ab411ecf9c55e6310ed2f5
abhi5hek001 <sahayabhishek.edu@gmail.com>
2025-08-24 16:16:42 +0530

    added ecosystem.config.js file

--- frontend/src/pages/adminDashboard/adminHome.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import AdminHome from "@/components/adminDashboard/AdminHome";
-
-const AdminHomePage = () => {
-  return <AdminHome />;
-};
-
-export default AdminHomePage; 
