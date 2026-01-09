3ecbfdbbd192dc5a460550b76f3ae8ffd3ea048b
abhi5hek001 <sahayabhishek.edu@gmail.com>
2026-01-09 15:23:30 +0530

    new version

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const app = require('./app'); // Import pre-configured app
-
-// Just start the server (safe for production & local use)
-app.listen(process.env.PORT || 3001, "0.0.0.0", () => {
-  console.log(`Server is Running on http://localhost:${process.env.PORT}`);
-  console.log(`Api Doc http://localhost:${process.env.PORT}/api-docs`);
-});
-
